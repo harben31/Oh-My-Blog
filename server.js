915095fa5f5c18c974c41bcc77f2d58f4ee1de28
Benjamin Harris <harben31@gmail.com>
2021-05-27 08:43:44 -0700

    added compression npm

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const compression = require('compression');
 
 const helpers = require('./utils/helpers.js');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -31,6 +33,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression())
 
 app.use(routes);
 
